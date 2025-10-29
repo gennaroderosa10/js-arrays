62987c8a088ff8088ab5be60797f98e6a606c45d
gennaroderosa10 <gennaroderosa5@gmail.com>
2025-10-29 15:49:11 +0100

    feat: da 1 a 6 done

--- 01-arrays/script.js
+++ 01-arrays/script.js
@@ -10,20 +10,36 @@ const teachers = [
 
 // 1. Utilizzando un indice sull'array teachers, cambia il valore della
 // variabile fourthTeacher qui sotto in modo che sia il quarto insegnante elencato nell'array
-const fourthTeacher = null;
+const fourthTeacher = teachers[3];
+console.log(fourthTeacher);
+
 
 // 2. Sostituisci il quinto insegnante nell'array teachers con 'Patrick'
+teachers[4] = "Patrick"
+console.log(teachers);
 
 // 3. Rimuovi l'ultimo insegnante dall'array e salvalo nella variabile lastTeacher qui sotto
-const lastTeacher = null;
+const lastTeacher = teachers[6];
+console.log(lastTeacher);
+teachers.splice(6.6);
+console.log(teachers);
 
 // 4. Rimuovi il primo insegnante dall'array e salvalo nella variabile firstTeacher qui sotto
-const firstTeacher = null;
+const firstTeacher = teachers[0];
+console.log(firstTeacher);
+teachers.shift();
+console.log(teachers);
+
+// si può usare anche teachers.pop()
+
 
 // 5. Aggiungi un insegnante di nome 'Vanessa' alla fine dell'array teachers
+teachers.push("Vanessa");
+console.log(teachers);
 
 // 6. Aggiungi un insegnante di nome 'Sarah' all'inizio dell'array teachers
-
+teachers.unshift("Sarah");
+console.log(teachers);
 // 7. Trova l'indice dell'insegnante 'Lewis' nell'array teachers
 // e salvalo nella variabile lewisIndex
 const lewisIndex = null;
